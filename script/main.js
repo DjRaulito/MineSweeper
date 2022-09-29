0133f864873bd1b27d4046f3cb15fd0a17a2c82c
DjRaulito <rmolina2003.rm@gmail.com>
2022-09-29 11:12:13 +0200

    First Commit BDD Scenario: Default Board

--- script/main.js
+++ script/main.js
@@ -1,26 +1,30 @@
 window.onload = function () {
-    CreateBoard();
-}
-let num = 8;
-function CreateBoard() {
-    // num = document.getElementById("num").value;
-    let board = document.querySelector("#board");
-    while (board.firstChild) {
-      board.removeChild(board.firstChild);
-    }
-    for (let c = 0; c < num; c++) {
-      let columnDiv = document.createElement("div");
-      columnDiv.setAttribute("id", "column" + (c + 1));
-      columnDiv.classList.add("column");
-      board.append(columnDiv);
-      for (let f = 0; f < num; f++) {
-        let newDiv = document.createElement("div");
-        newDiv.setAttribute("id", f + 1 + "-" + (c + 1));
-        newDiv.setAttribute("data-testid", f + 1 + "-" + (c + 1));
-        newDiv.setAttribute("onclick", "CheckCell(id)");
-        newDiv.classList.add("cell");
-        document.querySelector("#column" + (c + 1)).append(newDiv);
-      }
-    }
+  if (window.location.search.includes("?")){
+    GenerateMockData();    
   }
-  
+  CreateBoard();
+  ShowBoard()
+};
+let columns = 8;
+let rows = 8;
+let arrayBoard;
+function CreateBoard() {
+  arrayBoard = new Array(rows);
+  for (let c = 0; c < columns; c++) {
+    arrayBoard[c] = new Array(columns);
+  }
+}
+function ShowBoard() {
+  for (let c = 0; c < columns; c++) {
+    let column = document.createElement("div");
+    column.setAttribute("id","column" +(c + 1));
+    document.getElementById("board").append(column);
+    for (let f = 0; f < rows; f++) {
+      arrayBoard[c][f] = document.createElement("div");
+      arrayBoard[c][f].setAttribute("id", f + 1 + "-" + (c + 1));
+      arrayBoard[c][f].setAttribute("data-testid", f + 1 + "-" + (c + 1));
+      arrayBoard[c][f].classList.add("hiddenCell");
+      document.getElementById("column" + (c + 1)).append(arrayBoard[c][f]);
+    }
+  }  
+}
--- tests/steps/minesweeper.steps.js
+++ tests/steps/minesweeper.steps.js
@@ -6,11 +6,15 @@ const url = "http://127.0.0.1:5501";
 const num = 8;
 // const url = ' https://djraulito.github.io/MineSweeperBDD/';
 
+	// Primero saber cuantos divs hay en todo el board y luego saber cuantos divs tienen la clase correspondiente y comparar
+	// si las 2 consultas dan el mismo numero o no y dar un true o false
+
 Given("the user opens the app", async () => {
   await page.goto(url);
 });
 
 Then("on the board all the cells should be hidden", async () => {
-	await page.locator(`[data-testid="${string}"]`); 
-	await page.locator('data-testid=');
+	let numBoardDivs = 64;
+	const numHiddenCellsBoard = await page.$$('[class="hiddenCell"]');
+	expect(numHiddenCellsBoard.length).toBe(numBoardDivs);
 });
